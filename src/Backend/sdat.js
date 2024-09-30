9cc12fefc8e7559abe1e83934fc8b28cf7a0850c
trachselr <trachselr@bzz.ch>
2024-09-30 11:21:48 +0200

    ADDED: basic structure sdat.js

---
+++ src/Backend/sdat.js
@@ -0,0 +1,15 @@
+/*
+  VERSION:              Robin Trachsel
+  DATE:                 30.09.2024
+  DESCRIPTION:          sdat route
+*/
+
+const express = require('express')
+const router = express.Router()
+
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
+
+router.get('', (req, res) => {
+    res.json({ message: 'sdat test' })
+})
