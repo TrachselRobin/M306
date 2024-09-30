b54b8129bad764833264a48b6222fb73c7515980
non euclidean <spilkerj@bzz.ch>
2024-09-30 11:50:53 +0200

    initial xml to csv

--- src/Backend/esl.js
+++ src/Backend/esl.js
@@ -5,6 +5,8 @@
 */
 
 const express = require('express')
+const {readFileSync} = require("node:fs");
+const {parse} = require("fast-csv");
 const router = express.Router()
 
 router.use(express.json())
@@ -13,3 +15,35 @@ router.use(express.urlencoded({ extended: true }))
 router.get('', (req, res) => {
     res.json({ message: 'esl test' })
 })
+
+// convert to csv function
+function XML_to_CSV(XML_path, CSV_path) {
+    const xmlData = readFileSync(XML_path, 'utf-8');
+
+    const jsonData = parse(xmlData, {
+        ignoreAttributes: false,
+        attributeNamePrefix: ""
+    });
+
+    const timePeriodEnd = jsonData.ESLBillingData.Meter.TimePeriod.end;
+    const valueRows = jsonData.ESLBillingData.Meter.ValueRow;
+    const rows = valueRows.map(row => ({
+        timePeriodEnd,            // The end time from TimePeriod
+        obis: row.obis,            // OBIS code
+        valueTimeStamp: row.valueTimeStamp || '', // The timestamp if available
+        value: row.value,          // The meter value
+        status: row.status         // The status value
+    }));
+
+    if (!CSV_path) {
+    let csvString = '';
+    cvsWriter
+        .toString(rows, {headers: true})
+        .then(data => {
+            csvString = data;
+            console.log(csvString);
+        })
+        .catch(err => {'Error with CSV string'})
+    }
+    return csv
+}
