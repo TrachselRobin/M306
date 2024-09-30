f4d12205ca203f90aa44c9e50c71d69d19e87c06
trachselr <trachselr@bzz.ch>
2024-09-30 16:02:24 +0200

    ADDED: sql.js

---
+++ src/Backend/sql.js
@@ -0,0 +1,47 @@
+/*
+  VERSION:              Robin Trachsel
+  DATE:                 30.09.2024
+  DESCRIPTION:          MySQL database connection
+*/
+
+const express = require('express');
+const MYSQL = require('mysql');
+
+async function sqlQuery(SQL) {
+    return new Promise((resolve, reject) => {
+        const CONNECTION = MYSQL.createConnection({
+            host: process.env.MYSQL_HOST || "localhost",
+            database: process.env.MYSQL_DATABASE || "energieagentur_buenzli",
+            user: process.env.MYSQL_USER || "root", // user auf MySQL Workbench ersichtlich
+            password: process.env.MYSQL_PWD || "1234" // Ein Passwort, das auf MySQL Workbench gesetzt wurde
+        });
+
+        CONNECTION.connect((err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            CONNECTION.query(SQL, (error, results, fields) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+                CONNECTION.end();
+            });
+        });
+    });
+}
+
+module.exports = sqlQuery;
+
+/*
+    Use in async function
+
+    router.get('/users', async (req, res) => {
+        const SQL = "SELECT * FROM users";
+        const result = await sqlQuery(SQL);
+        res.send(result);
+    });
+*/
